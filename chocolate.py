1ceba99d63b5758cc9b3e7952f3451cb3876b38f
Gerwan <gerwantaylor@gmail.com>
2024-04-04 21:44:26 -0700

    While loop simple

---
+++ chocolate.py
@@ -0,0 +1,14 @@
+# basic while loop 
+count = 0
+while count < 5:
+    print(count)
+    count += 1
+
+# while loop with else
+count = 0
+while count < 5:
+    print(count)
+    count += 1
+else:
+    print("count value reached %d" %(count))
+    
