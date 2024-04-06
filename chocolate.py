6f2a86878107bcea5d9904ea14f4fd33315d6a00
whiffoflevon <159498861+whiffoflevon@users.noreply.github.com>
2024-04-05 21:47:35 -0700

    Update chocolate.py
    added lists and dictionaries

--- chocolate.py
+++ chocolate.py
@@ -1,14 +1,30 @@
-# basic while loop 
-count = 0
-while count < 5:
-    print(count)
-    count += 1
+# allow user to add a new fragrance to the list
+def add_fragrance(fragrance_list, name, house, price, size):
+    fragrance_list.append({"name": name, "house": house, "price": price, "size": size})
+    return fragrance_list
 
-# while loop with else
-count = 0
-while count < 5:
-    print(count)
-    count += 1
-else:
-    print("count value reached %d" %(count))
+fragrance_list =[
+    {"name": "Naxos", "house": "Xerjoff", "price": "$315.00", "size": "3.4 oz"}, 
+    {"name":"Oud Noir", "house":"Aaron Terence Hughes", "price": "$200.00", "size":  "1.7 oz"},
+    {"name":"Carlisle", "house": "Parfums de Marly", "price": "$275.00", "size":  "4.2 oz"}
+]
+
+while True:
+    name = input("Name: ")
+    if name == "exit":
+        break
+    
+    house = input("House: ")
+    price = input("Price: ")
+    size = input("Size: ")
+
+    add_fragrance(fragrance_list, name, house, price, size)
+
+
+for frag in fragrance_list: 
+        print(frag["name"] + " by " + frag["house"] + " is " + frag["price"] + " for " + frag["size"] + ".")
+
+
+
+
     
