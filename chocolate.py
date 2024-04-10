a8ccb804abaa75eff4784d8436688fd45c3a6dc2
whiffoflevon <159498861+whiffoflevon@users.noreply.github.com>
2024-04-09 23:48:15 -0700

    Update chocolate.py

--- chocolate.py
+++ chocolate.py
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-=======
-# allow user to add a new fragrance to the list
-def add_fragrance(fragrance_list, name, house, price, size):
-    fragrance_list.append({"name": name, "house": house, "price": price, "size": size})
-    return fragrance_list
-
-
->>>>>>> d65ef8b73ab0555a39885482461f04ee3ff4497d
 fragrance_list =[
     {"name": "Naxos", "house": "Xerjoff", "price": "$315.00", "size": "3.4 oz"}, 
     {"name":"Oud Noir", "house":"Aaron Terence Hughes", "price": "$200.00", "size":  "1.7 oz"},
@@ -17,7 +8,6 @@ def add_fragrance(fragrance_list, name, house, price, size):
     fragrance_list.append({"name": name, "house": house, "price": price, "size": size})
     return fragrance_list
 
-<<<<<<< HEAD
 
 def search_fragrance(): 
     try:
@@ -32,47 +22,22 @@ def search_fragrance():
     except ValueError:
         print("Please enter a valid string.")
         return search_fragrance()
-=======
->>>>>>> d65ef8b73ab0555a39885482461f04ee3ff4497d
 
-i=0
-while i < 1:
-    print("""
-    Welcome to the fragrance store! Press (1) To Add a fragrance | Press (2) To Search for a fragrance 
-    | Press (3) To Delete a fragrance | (4) Exit""")
-    print()
-    try:
-     ops=int(input("Select: "))
-     if ops == 4:
-         break
-     elif ops == 1:
-            name = input("Name: ")
-            house = input("House: ")
-            price = input("Price: ")
-            size = input("Size: ")
-            add_fragrance(fragrance_list, name, house, price, size)
-           
-         
-    except ValueError:
-        print("Please enter a valid integer.")
-        continue
 
 def delete_fragrance():
     try:
         delete=input("Which fragrance would you like to delete?: ")
         for frag in fragrance_list:
             if frag["name"]==delete:
-                frag.pop("name")
-                print("Fragrance has been removed.")
-            elif delete == "exit":
+                frag.remove(frag)
+                print(frag["name"] + " has been removed.")
                 break
-            else:
-                print("Fragrance not found.")
-                return delete_fragrance()
+        else:
+            print("Fragrance not found.")
+            return delete_fragrance()
     except ValueError:
         print("Please enter a valid string.")
         return delete_fragrance()
-        
 
 i=0
 while i < 1:
@@ -92,8 +57,7 @@ while i < 1:
             add_fragrance(fragrance_list, name, house, price, size)
      elif ops == 2:
          search_fragrance()
-     elif ops == 3:
-         delete_fragrance()      
+           
          
     except ValueError:
         print("Please enter a valid integer.")
@@ -103,10 +67,5 @@ for frag in fragrance_list:
         print(frag["name"] + " by " + frag["house"] + " is " + frag["price"] + " for " + frag["size"] + ".")
 
 
-<<<<<<< HEAD
-=======
-    
-
->>>>>>> d65ef8b73ab0555a39885482461f04ee3ff4497d
     
  
